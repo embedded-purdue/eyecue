f50acceaa3fb15adc3e120da5d9fcb0f2f1db2b9
Luca DalCanto <111649024+Luca-Skyline@users.noreply.github.com>
2025-11-02 10:26:47 -0500

    improve cursor controller
    This script should now work for any screen which lies on the plane orthogonal to the ray from the eye to the center of the screen.

--- CursorController.py
+++ CursorController.py
@@ -2,19 +2,45 @@
 """
 Created on Thu Sep 25 17:43:39 2025
 
-@author: Luca DalCanto
+@author:    Luca DalCanto, Purdue University
+            on behalf of the Eyecue team of ES@P
 
 The purpose of this class is to take the 9-dot calibration information,
 position of the eye at any given time, and head angles at any given 
 time (from gyro) to calculate the correct position of the mouse on the
 screen.
 
-Assumptions:
-    - face is centered compared to screen left-to-right and camera left-to-right
-    (this assumption only holds up using a webcam, not glasses likely)
-    - face is centered up-and-down on those metrics too
-    - this will be fixed by Sunday so it can accomodate for ANY screen orthagonal to your vision
+How to use:
     
+    - import this file ("import CursorController") or something idk
+    
+    - create an instance of this class after calibration:
+            
+        "controller = CursorController(...parameters here...)"
+            
+        parameters of instantitaion:
+            - leftAngle: nine-dot left calibration horizontal angle in degrees
+            - rightAngle: nine-dot right calibration horizontal angle in degrees
+            - topAngle: nine-dot top calibration vertical angle in degrees
+            - bottomAngle: nine-dot bottom calibration vertical angle in degrees
+            - gyroH: initial horizontal angle of gyro
+            - gyroV: intial vertial angle of gyro
+            - frameRate: intended number of updates per second
+                
+    - every frame, call the update function:
+        
+        "controller.update_target(...parameters here...)"
+        
+        parameters of update_target:
+            - angleH: current horizontal eye angle in degrees
+            - angleV: current vertical eye angle in degrees
+            - gyroH: current horizontal gyro angle
+            - gyroV: current vertical gyro angle
+    
+Areas for improvement:
+    - calculates correct mapping with screen distance and screen size using the 
+    horizontal angles only -> mapping for vertical angles may be imperfect.
+    - 
 
 
 """
@@ -26,8 +52,8 @@ import numpy as np
 class CursorController:
 
     # Constructor method
-    def __init__(self, leftAngle, rightAngle, topAngle, bottomAngle, gyroH, gyroV):
-        self.frameRate = 2
+    def __init__(self, leftAngle, rightAngle, topAngle, bottomAngle, gyroH, gyroV, frameRate):
+        self.frameRate = frameRate
         self.screenWidth, self.screenHeight = pyautogui.size()
         
         leftAngle *= math.pi / 180
@@ -37,6 +63,9 @@ class CursorController:
         
         self.screenDistance = self.screenWidth / (2 * math.tan(abs(leftAngle - rightAngle)/2))
         self.gyroCenter = [gyroH, gyroV]
+        self.eyeCenter = [(leftAngle + rightAngle) / 2, (topAngle + bottomAngle) / 2]
+        
+        print(self.eyeCenter[1])
         
     
     # takes angle of vision (angle H - horizontal, angleV - vertical) in degrees
@@ -46,6 +75,10 @@ class CursorController:
         # convert horizontal and vertical rotations to radians
         angleH *= math.pi / 180
         angleV *= -math.pi / 180
+        
+        # account for initial calibration
+        angleH -= self.eyeCenter[0]
+        angleV += self.eyeCenter[1]
         
         # account for head rotation
         angleH += (gyroH - self.gyroCenter[0]) * math.pi / 180
@@ -69,5 +102,5 @@ class CursorController:
         
 
 
-# control = CursorController(-28.4, 28.4, 16.3, -16.3, 0, 0)
-# control.update_target(-28.4, -16.3, 0, 0)
+# control = CursorController(0, 56.8, 16.3, -16.3, 0, 0)
+# control.update_target(0, -17.3, 0, 0)
